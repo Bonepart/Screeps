5257074d9fbb3964fb71232abff2997a72b7fda8
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-20 16:26:37 -0500

    use grunt to push to screeps server

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+    var config = require('./.screeps.json')
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var token = grunt.option('token') || config.token;
+
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: email,
+                token: token,
+                branch: branch,
+                //server: 'season'
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
+    });
+}
