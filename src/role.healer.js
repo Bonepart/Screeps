b1708ee520aa4a03b6ec76ca49bd20d83114f0c0
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-22 13:27:44 -0500

    add run function

--- src/role.healer.js
+++ src/role.healer.js
@@ -1,7 +1,20 @@
+var processRenewal = require('process.renewal');
 var helper = require('helper');
 var pathing = require('pathing');
 
 var roleHealer = {
 
+    /** @param {Creep} creep **/
+    run: function (creep) {
+        if(processRenewal.renew(creep)){ return };
+
+        let creepInNeed = creep.pos.findClosestByPath(LOOK_CREEPS, { 
+            filter: (creep) => {return creep.hits < creep.hitsMax}});
+        if (creepInNeed){
+            if (creep.heal(creepInNeed) == ERR_NOT_IN_RANGE){
+                creep.moveTo(creepInNeed, {visualizePathStyle: {stroke: '#000000'}});
+            }
+        }
+    }
 }
 module.exports = roleHealer;
