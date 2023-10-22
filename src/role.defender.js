52f3f972e626abbf4fd7956bcdd4630a767766a9
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-22 10:57:14 -0500

    update to have defenders move to nearest rampart

--- src/role.defender.js
+++ src/role.defender.js
@@ -5,24 +5,25 @@ var roleDefender = {
     /** @param {Creep} creep **/
     run: function(creep){
         if(processRenewal.renew(creep)){ return };
-        var dFlag = creep.room.find(FIND_FLAGS, {filter: (flag) => {return flag.name == "Defenders"}});
-        var targets = creep.room.find(FIND_HOSTILE_CREEPS, {filter: (hostile) => {
-            return hostile.owner.username != "Source Keeper";
-        }});
-        //console.log(`${creep.name} sees ${targets.length} hostiles`);
-        if (targets.length > 0){
-            let result = creep.attack(targets[0]);
+        let dFlag = creep.room.find(FIND_FLAGS, {filter: (flag) => {return flag.name == "Defenders"}});
+        let searchFromPos = creep.pos;
+        if (dFlag.length > 0) { searchFromPos = dFlag.pos }
+        let rampart = searchFromPos.findClosestByPath(FIND_MY_STRUCTURES, { filter: (struct) => {return struct.structureType == STRUCTURE_RAMPART}});
+        let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+        if (target){
+            let result = creep.attack(target);
             switch(result){
                 case OK:
-                    console.log(`${creep.name} attacked ${targets[0].name}!`);
+                    console.log(`${creep.name} attacked ${target.name}! (${target.hits}/${target.hitsMax})`);
                     break;
                 case ERR_NOT_IN_RANGE:
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff0000'}});
                     break;
                 default:
-                    console.log(`${creep.name} tried to attck, got error ${result}`);
+                    console.log(`${creep.name} tried to attck ${target.name}, got error ${result}`);
             }
-        } else if (dFlag.length > 0) {creep.moveTo(dFlag[0], {visualizePathStyle: {stroke: '#ffffff'}})};
+        } 
+        if (rampart) {creep.moveTo(rampart, {visualizePathStyle: {stroke: '#ffffff'}})};
     }
 }
 module.exports = roleDefender;
