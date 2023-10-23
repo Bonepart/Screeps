741b06a416988f922412343603553d45eee67bcd
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-23 01:08:31 -0500

    finished adding Tier 2 bodytypes

--- src/constants.bodytype.js
+++ src/constants.bodytype.js
@@ -5,7 +5,8 @@ exports.harvester = [
 ];
 exports.upgrader = [
     [WORK, CARRY, MOVE, MOVE], //Cost 250
-    [WORK, WORK, WORK, CARRY, MOVE, MOVE] //Cost 450
+    [WORK, WORK, WORK, CARRY, MOVE, MOVE], //Cost 450
+    [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE] //Cost 700
 ];
 exports.builder = [
     [WORK, CARRY, MOVE, MOVE], //Cost 250
@@ -14,7 +15,8 @@ exports.builder = [
 ];
 exports.maintenance = [
     [WORK, CARRY, MOVE, MOVE], //Cost 250
-    [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] //Cost 500
+    [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], //Cost 500
+    [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] //Cost 700
 ];
 exports.defender = [
     [TOUGH, TOUGH, ATTACK, MOVE, MOVE, MOVE], //Cost 250
@@ -23,9 +25,14 @@ exports.defender = [
 ];
 exports.healer = [
     [ HEAL, MOVE], //Cost 300
-    [ RANGED_ATTACK, HEAL, MOVE, MOVE] //Cost 500
+    [ RANGED_ATTACK, HEAL, MOVE, MOVE], //Cost 500
+    [ TOUGH, TOUGH, HEAL, HEAL, MOVE, MOVE, MOVE, MOVE] //Cost 720
 ];
 
+//Explorer Bodytypes
 exports.longhauler = [
     [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] //Cost 800
 ];
+exports.claimer = [
+    [TOUGH, CLAIM, MOVE, MOVE] //Cost 710
+]
