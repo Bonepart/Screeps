019c16ff7089fa3da738d79d5a7b1f326cd96dac
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-22 20:43:02 -0500

    move bodytype definitions into it's own file

---
+++ src/constants.bodytype.js
@@ -0,0 +1,32 @@
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+        enumerable: true
+    });
+}
+
+
+exports.harvester = [
+    [WORK, CARRY, MOVE, MOVE], //Cost 250
+    [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE] //Cost 500
+];
+exports.upgrader = [
+    [WORK, CARRY, MOVE, MOVE], //Cost 250
+    [WORK, WORK, WORK, CARRY, MOVE, MOVE] //Cost 450
+];
+exports.builder = [
+    [WORK, CARRY, MOVE, MOVE], //Cost 250
+    [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] //Cost 500
+];
+exports.maintenance = [
+    [WORK, CARRY, MOVE, MOVE], //Cost 250
+    [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] //Cost 500
+];
+exports.defender = [
+    [TOUGH, TOUGH, ATTACK, MOVE, MOVE, MOVE], //Cost 250
+    [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE] //Cost 500
+];
+exports.healer = [
+    [ HEAL, MOVE], //Cost 300
+    [ RANGED_ATTACK, HEAL, MOVE, MOVE] //Cost 500
+];
