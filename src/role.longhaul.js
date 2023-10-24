b0e9c22cc1169c1a1ab5dc431c80485c2ece8f53
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-24 15:42:02 -0500

    change to use import container

--- src/role.longhaul.js
+++ src/role.longhaul.js
@@ -56,30 +56,40 @@ let roleLonghaul = {
         }
         else {
             let originRoom = Game.rooms[creep.memory.originRoom];
-            let targets = originRoom.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) && 
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            let importContainer = originRoom.find(FIND_FLAGS, { filter: (flag) => { return flag.name = "Import"}});
+            if (importContainer.length > 0) {importContainer = importContainer.pos.lookFor(LOOK_STRUCTURES, { 
+                filter: (struct) => { return struct.structureType == STRUCTURE_CONTAINER}})};
+            if (importContainer.length > 0) {
+                if(creep.transfer(importContainer[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(importContainer[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else {
+                let targets = originRoom.find(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_EXTENSION ||
+                                    structure.structureType == STRUCTURE_SPAWN) && 
+                                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                        }
+                });
+                targets = targets.concat(originRoom.find(FIND_STRUCTURES, {
+                    filter: (structure) => { return (structure.structureType == STRUCTURE_TOWER ) && 
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}}));
+                targets = targets.concat(originRoom.find(FIND_STRUCTURES, {
+                    filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER ) && 
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}}));
+                let parkingFlag = originRoom.find(FIND_FLAGS, {filter: (flag) => {return flag.name == "Parking"}});
+                let buildables = originRoom.find(FIND_CONSTRUCTION_SITES);
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
-            });
-            targets = targets.concat(originRoom.find(FIND_STRUCTURES, {
-                filter: (structure) => { return (structure.structureType == STRUCTURE_TOWER ) && 
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}}));
-            targets = targets.concat(originRoom.find(FIND_STRUCTURES, {
-                filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER ) && 
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}}));
-            let parkingFlag = originRoom.find(FIND_FLAGS, {filter: (flag) => {return flag.name == "Parking"}});
-            let buildables = originRoom.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            } else if(buildables.length > 0) {
-                if(creep.build(buildables[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(buildables[0], {visualizePathStyle: {stroke: '#0000ff'}});
-                }
-            } else if (parkingFlag.length > 0) {creep.moveTo(parkingFlag[0], {visualizePathStyle: {stroke: '#ffffff'}});}
+                } else if(buildables.length > 0) {
+                    if(creep.build(buildables[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(buildables[0], {visualizePathStyle: {stroke: '#0000ff'}});
+                    }
+                } else if (parkingFlag.length > 0) {creep.moveTo(parkingFlag[0], {visualizePathStyle: {stroke: '#ffffff'}});}
+            }
         }
     }
 }
