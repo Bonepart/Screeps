e037a821527d94516e64ddf7e15c81b611d80b87
Joshua Bartz <28548170+Bonepart@users.noreply.github.com>
2023-10-24 09:49:07 -0500

    expand on room states

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,10 @@ global.ROLE_CLAIMER = 'claimer';
 global.ZOMBIE = 'zombie';
 
 //Room States
-global.ROOM_HOSTILE = -1;
+global.ROOM_HOSTILE_SAFE = -3
+global.ROOM_HOSTILE = -2
+global.ROOM_HOSTILE_RESERVED = -1;
 global.ROOM_NEUTRAL = 0;
 global.ROOM_RESERVED = 1;
 global.ROOM_OWNED = 2;
+global.ROOM_OWNED_SAFE = 3;
