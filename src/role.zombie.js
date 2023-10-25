9e01d4071e8bc2d52afd3884be57328c389be725
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-24 23:55:32 -0500

    fixed so it works outside of main room

--- src/role.zombie.js
+++ src/role.zombie.js
@@ -3,11 +3,12 @@ let roleZombie = {
 
     /** @param {Creep} creep */
     run: function(creep){
-        let spawns = creep.room.find(FIND_MY_SPAWNS);
-        if (spawns.length > 0){
-            if (spawns[0].recycleCreep(creep) == ERR_NOT_IN_RANGE){
-                creep.moveTo(spawns[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        let spawner = null;
+        for (let i in Game.spawns){
+            spawner = Game.spawns[i];
+        }
+        if (spawner.recycleCreep(creep) == ERR_NOT_IN_RANGE){
+            creep.moveTo(spawner, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
     }
 }
