5ba5269d8b212a616a9b2d109d10e856d6beadb9
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-25 19:13:25 -0500

    add missing logic

--- src/role.gofer.js
+++ src/role.gofer.js
@@ -67,6 +67,11 @@ let roleGofer = {
             filter: (structure) => { return (structure.structureType == STRUCTURE_STORAGE ) && 
                 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
             }}));
+        if(targets.length > 0) {
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
     }
   }
 }
