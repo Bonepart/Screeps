29ba8d45c82225733289c02b627c1ee0874b9235
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-27 18:15:05 -0500

    initial commit

---
+++ src/structure.link.js
@@ -0,0 +1,18 @@
+
+let structureLink = {
+
+/** @param {StructureLink} fromLink **/
+    run: function (fromLink){
+        for (let i in fromLink.room.memory.links) {
+            if (fromLink.id == fromLink.room.memory.links[i].from) {
+                let toLink = Game.getObjectById(fromLink.room.memory.links[i].to);
+                if (fromLink.cooldown == 0 && fromLink.store.getUsedCapacity(RESOURCE_ENERGY) >= 100) {
+                    let result = fromLink.transferEnergy(toLink);
+                    console.log(`Link to Link Transfer: ${result}`);
+                }
+                return;
+            }
+        }
+    }
+}
+module.exports = structureLink;
