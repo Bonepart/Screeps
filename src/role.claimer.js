0355b5b63bcee18f6e713135c4d6852c7d4b9411
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-26 20:43:44 -0500

    add ability to claim controllers

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -4,19 +4,25 @@ let roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function(creep){
-        
-        assignedRoom = creep.memory.assignedRoom;
-
         let destRoom = Game.rooms[creep.memory.assignedRoom];
         if (destRoom == undefined){
             let newRoom = new RoomPosition(24, 24, creep.memory.assignedRoom);
             creep.moveTo(newRoom, {visualizePathStyle: {stroke: '#ffffff'}});
         }
         else {
-            if (destRoom.memory.missionaryID == 'spawning') { destRoom.memory.missionaryID = creep.id }
-            let result = creep.reserveController(destRoom.controller);
-            //if (destRoom.controller.isActive()){ result = creep.claimController(destRoom.controller)}
-            //else {result = creep.reserveController(destRoom.controller)}
+            let result = creep.claimController(destRoom.controller);
+            switch (result){
+                case OK:
+                    return;
+                case ERR_NOT_IN_RANGE:
+                    creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                    return;
+                case ERR_GCL_NOT_ENOUGH:
+                    break;
+                default:
+                    console.log(`${creep.name} had a claim error: ${result}`);
+            }            
+            result = creep.reserveController(destRoom.controller);
             switch (result){
                 case OK:
                     break;
@@ -24,10 +30,9 @@ let roleClaimer = {
                     creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
                     break;
                 default:
-                    console.log(`${creep.name} had an error: ${result}`);
+                    console.log(`${creep.name} had a reserve error: ${result}`);
             }
         }
-        
     }
 }
 module.exports = roleClaimer;
