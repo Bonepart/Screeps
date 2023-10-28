aee8058416683c6a3018f7763d07a564db470af3
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-28 02:08:29 -0500

    fix transfer bug

--- src/structure.link.js
+++ src/structure.link.js
@@ -1,3 +1,4 @@
+let helper = require('helper');
 
 let structureLink = {
 
@@ -6,9 +7,10 @@ let structureLink = {
         for (let i in fromLink.room.memory.links) {
             if (fromLink.id == fromLink.room.memory.links[i].from) {
                 let toLink = Game.getObjectById(fromLink.room.memory.links[i].to);
-                if (fromLink.cooldown == 0 && fromLink.store.getUsedCapacity(RESOURCE_ENERGY) >= 100) {
+                if (fromLink.cooldown == 0 && fromLink.store.getUsedCapacity(RESOURCE_ENERGY) >= 100 &&
+                    toLink.store.getFreeCapacity(RESOURCE_ENERGY) >= 97) {
                     let result = fromLink.transferEnergy(toLink);
-                    console.log(`Link to Link Transfer: ${result}`);
+                    //console.log(`Link to Link Transfer: ${result}`);
                 }
                 return;
             }
