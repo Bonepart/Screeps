9c50a9e39cded6a6cd7e2913f4491a0ecd3f9d2d
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-29 10:29:24 -0500

    fix pathfinding

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -5,35 +5,37 @@ let roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function(creep){
-        if (creep.memory.assignedRoom) {
+
+        let destRoom = Game.rooms[creep.memory.assignedRoom];
+        if (destRoom === undefined) {
             if (creep.room.name != creep.memory.assignedRoom) {
                 common.moveToAssignedRoom(creep);
                 return;
             }
         }
-
-        let destRoom = Game.rooms[creep.memory.assignedRoom];
-        let result = creep.claimController(destRoom.controller);
-        switch (result){
-            case OK:
-                return;
-            case ERR_NOT_IN_RANGE:
-                creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-                return;
-            case ERR_GCL_NOT_ENOUGH:
-                break;
-            default:
-                console.log(`${creep.name} had a claim error: ${result}`);
-        }            
-        result = creep.reserveController(destRoom.controller);
-        switch (result){
-            case OK:
-                break;
-            case ERR_NOT_IN_RANGE:
-                creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-                break;
-            default:
-                console.log(`${creep.name} had a reserve error: ${result}`);
+        else {
+            let result = creep.claimController(destRoom.controller);
+            switch (result){
+                case OK:
+                    return;
+                case ERR_NOT_IN_RANGE:
+                    creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                    return;
+                case ERR_GCL_NOT_ENOUGH:
+                    break;
+                default:
+                    console.log(`${creep.name} had a claim error: ${result}`);
+            }            
+            result = creep.reserveController(destRoom.controller);
+            switch (result){
+                case OK:
+                    break;
+                case ERR_NOT_IN_RANGE:
+                    creep.moveTo(destRoom.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                    break;
+                default:
+                    console.log(`${creep.name} had a reserve error: ${result}`);
+            }
         }
     }
 }
