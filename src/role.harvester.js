8b8e70b06327dfc7bee566a8e88bec8bc63f9afe
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-04 17:29:25 -0500

    move logic to find transfer energy target->common

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -31,23 +31,10 @@ let roleHarvester = {
             }
         }
         else {
-            let targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) && 
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                    }
-            });
-            targets = targets.concat(creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => { return (structure.structureType == STRUCTURE_STORAGE ||
-												 structure.structureType == STRUCTURE_CONTAINER ) &&
-                    							 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}
-				})
-			);
-            
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            let targetSpawn = pathing.findClosestEnergyConsumer(creep.pos);
+            if (targetSpawn){
+                if(creep.transfer(targetSpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targetSpawn, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
 				return;
             }
@@ -60,5 +47,4 @@ let roleHarvester = {
         }
 	}
 };
-
 module.exports = roleHarvester;
