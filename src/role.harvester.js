2cd579bccbd3b407cb3240cacd234ec80b1644f1
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-04 16:58:18 -0500

    add logic so can look for loose energy

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -6,7 +6,7 @@ let helper = require('helper');
 let roleHarvester = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function(creep, hasLooseEnergy) {
         if (creep.memory.assignedRoom) {
             if (creep.room.name != creep.memory.assignedRoom) {
                 common.moveToAssignedRoom(creep);
@@ -24,6 +24,7 @@ let roleHarvester = {
 	    if(creep.memory.harvesting) {
             if(processRenewal.renew(creep)){ return };
 
+            if (hasLooseEnergy && common.getLooseEnergy) { return }
             let searchTarget = pathing.findClosestSource(creep.pos)
             if(creep.harvest(searchTarget) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(searchTarget, {visualizePathStyle: {stroke: '#ffaa00'}});
@@ -43,7 +44,6 @@ let roleHarvester = {
                     							 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}
 				})
 			);
-            let parkingFlag = creep.room.find(FIND_FLAGS, {filter: (flag) => {return flag.name == "Parking"}});
             
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
@@ -56,7 +56,7 @@ let roleHarvester = {
                 if(creep.build(buildables[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(buildables[0], {visualizePathStyle: {stroke: '#0000ff'}});
                 }
-            } else if (parkingFlag.length > 0) {creep.moveTo(parkingFlag[0], {visualizePathStyle: {stroke: '#ffffff'}});}
+            }
         }
 	}
 };
