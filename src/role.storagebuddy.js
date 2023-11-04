ae196d0ac4dfa8caad562360c5de8dcdee7aed17
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-04 14:33:53 -0500

    initial commit

---
+++ src/role.storagebuddy.js
@@ -0,0 +1,11 @@
+let common = require('common.logic');
+let helper = require('helper');
+
+let roleStorageBuddy = {
+    
+    /** @param {Creep} creep **/
+    run: function(creep){
+  
+    }
+}
+module.exports = roleStorageBuddy;
