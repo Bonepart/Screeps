135bba14ac79dbf45b321bbf2fbb67f98ed727eb
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-04 16:07:18 -0500

    only spawn storagebuddy if link present

--- src/process.creeps.js
+++ src/process.creeps.js
@@ -78,9 +78,10 @@ let processCreeps = {
                 else if (storageBuddyList[i] == 0){
                     let energyStorage = Game.rooms[i].find(FIND_STRUCTURES, {
                         filter: (structure) => { return structure.structureType == STRUCTURE_STORAGE }}).length;
-                    if (energyStorage > 0){
+                    if (energyStorage > 0 && Memory.rooms[i].links > 0){
                         body = bodytype.storagebuddy;
-                        if (spawnCreep(spawnIndex, ROLE_STORAGEBUDDY, body, -1, i)) { return }
+                        if (spawnCreep(spawnIndex, ROLE_STORAGEBUDDY, body, -1
+                            , i)) { return }
                     }
                     
                 }
