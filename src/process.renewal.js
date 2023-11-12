8fe93c1949a0b676d15575f4bce0618f9cb57881
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-12 01:09:57 -0600

    disable renewed logging

--- src/process.renewal.js
+++ src/process.renewal.js
@@ -8,7 +8,7 @@ let processRenewal = {
             //console.log(`Creep ${creep.name} (T${creep.memory.tier}) returning to spawn to renew (${creep.ticksToLive})`);
         } else if (creep.memory.renewing && creep.ticksToLive > 1300){
             creep.memory.renewing = false;
-            console.log(`Creep ${creep.name} renewed! (${creep.ticksToLive})`);
+            //console.log(`Creep ${creep.name} renewed! (${creep.ticksToLive})`);
         }
         if (creep.memory.renewing){
             let spawner = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
