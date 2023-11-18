dbf78669623bef5c35618ed83e607437891ada4c
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-11-18 08:03:10 -0600

    adjust repair list

--- src/logic.common.js
+++ src/logic.common.js
@@ -49,8 +49,11 @@ let commonLogic = {
         let containerRepairs = Game.rooms[roomName].find(FIND_STRUCTURES, { 
             filter: (structure) => { return structure.hits < structure.hitsMax && structure.structureType == STRUCTURE_CONTAINER}}
         );
-        let pendingRepairs = containerRepairs.concat(_.sortBy(Game.rooms[roomName].find(FIND_STRUCTURES, { 
-            filter: (structure) => { return  structure.hitsMax > 10000 && (structure.hits < structure.hitsMax * 0.66 || structure.hits < 25000) && structure.structureType == STRUCTURE_ROAD}}), (struct) => struct.hits)
+        let pendingRepairs = containerRepairs.concat(Game.rooms[roomName].find(FIND_STRUCTURES, { 
+            filter: (structure) => { return structure.hits < 10000 && structure.structureType == STRUCTURE_RAMPART}}
+        ));
+        pendingRepairs = pendingRepairs.concat(_.sortBy(Game.rooms[roomName].find(FIND_STRUCTURES, { 
+            filter: (structure) => { return  structure.hitsMax > 10000 && (structure.hits < 500000 || structure.hits < 25000) && structure.structureType == STRUCTURE_ROAD}}), (struct) => struct.hits)
         );
         pendingRepairs = pendingRepairs.concat(_.sortBy(Game.rooms[roomName].find(FIND_STRUCTURES, { 
             filter: (structure) => { return structure.hits < structure.hitsMax && 
